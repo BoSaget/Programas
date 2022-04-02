5561910967d5890aaa7f180cfc6d45dd8314eab1
Leon <leontejeda@gmail.com>
2022-04-01 21:48:02 -0600

    Actualizacion

---
+++ Python/String Split and Join.py
@@ -0,0 +1,10 @@
+def split_and_join(cadena):
+    #cadena = input("")
+
+    cadena = cadena.split(" ")
+    #print(cadena)
+
+    cadena = "-".join(cadena)
+    ##print(cadena)
+    
+    return cadena
