7b08d089319c22bdec2812db3dc6f50f0c5f28b9
Leon <leontejeda@gmail.com>
2022-04-28 00:12:19 -0500

    Finalmente acabe el Rangoligit add .git add .

--- Python/Alphabet Rangoli.py
+++ Python/Alphabet Rangoli.py
@@ -5,25 +5,58 @@ def print_rangoli(size):
     alto = (size*2)-1
     ancho = (size*4)-3
     mitad = ancho // 2
-    cadena = []
-    indice = 0
+    lista = []
+    indice_izq = mitad
+    indice_der = mitad
+    contador_medio= 0
+    contador = 0
     
     for i in range(0, ancho):
-        if(i == mitad):
-            cadena.append(chr(letra))
+        if (i == mitad ):
+            lista.append(chr(letra))
         else:
-            cadena.append("-")
-    
+            lista.append("-")
+            
     for i in range(0, alto):
-        for j in range(0, ancho):
-            if (indice == 0):
-                indice = indice + 1
-            else:
-                cadena[mitad- (indice * 2)] = chr(letra- indice)
-                indice = indice - 1
+        cadena = "".join(lista)
         
-        cadena = "".join(cadena)
+        if(i < alto//2):
+            contador_medio = contador_medio - 1
+            contador = contador + 1
+            lista[mitad] = chr(letra + contador_medio)
+            
+            indice_izq  =  indice_izq - 2
+            indice_der  =  indice_der + 2
+            for j in range(0, contador):
+                lista[indice_der - (2 * j)] = chr(letra - j)
+                lista[indice_izq + (2 * j)] = chr(letra - j)
+            
+                            
+        elif(i == alto//2):
+            contador_medio = contador_medio + 1
+            lista[mitad] = chr(letra + contador_medio)
+            
+            lista[indice_der] = "-"
+            lista[indice_izq] = "-"
+            for j in range(0, contador):
+                lista[indice_der - (2 * (j + 1 ))] = chr(letra - j) 
+                lista[indice_izq + (2 * (j + 1))] = chr(letra - j)
+            
+        else:
+            contador_medio = contador_medio + 1
+            contador = contador - 1
+            lista[mitad] = chr(letra + contador_medio)
+             
+            indice_izq  =  indice_izq + 2
+            indice_der  =  indice_der - 2
+           
+            lista[indice_der] = "-"
+            lista[indice_izq] = "-"
+            
+            for j in range(0, contador):
+                lista[indice_der - (2 * (j + 1 ))] = chr(letra - j) 
+                lista[indice_izq + (2 * (j + 1))] = chr(letra - j)
         print(cadena)
-
+    
 n = int(input())
 print_rangoli(n)
---
+++ Python/Capitalize!.py
@@ -0,0 +1,19 @@
+def solve(cadena):
+    cadena_nueva = list(cadena)
+    for i in range(0, len(cadena)):
+        if(i == 0):
+            if(cadena_nueva[i].isalpha() == True and cadena_nueva[i].isupper() == False):
+                letra = cadena_nueva[i]
+                cadena_nueva[i] = letra.upper()
+                
+        elif(cadena_nueva[i-1] == " "):
+            if(cadena_nueva[i].isalpha() == True and cadena_nueva[i].isupper() == False):
+                letra = cadena_nueva[i]
+                cadena_nueva[i] = letra.upper()
+                
+    cadena = "".join(cadena_nueva)
+    return cadena      
+    
+
+cadena = input("")
+solve(cadena)
