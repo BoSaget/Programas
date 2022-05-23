11d370b6e01dae69d5101dfe75dfa51c5b2268fc
Leon <leontejeda@gmail.com>
2022-05-23 14:23:53 -0500

    Actualizaicon codigo a 2 sensores

--- Python/IA - Micromouse/sensor.py
+++ Python/IA - Micromouse/sensor.py
@@ -5,22 +5,34 @@ from time import sleep
 import machine
 
 #Led de salida
-led_verde = machine.Pin(21, machine.Pin.OUT)
-led_rojo = machine.Pin(2, machine.Pin.OUT)
+led_der = machine.Pin(2, machine.Pin.OUT)
+led_izq = machine.Pin(12, machine.Pin.OUT)
 
 # ESP32
-sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
+sensor_der = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
+sensor_izq = HCSR04(trigger_pin=25, echo_pin=33, echo_timeout_us=10000)
 
 # ESP8266
 #sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
 
 while True:
-    distance = sensor.distance_cm()
-    if (distance > 5):
-        led_verde.on()
-        led_rojo.off()
+    distance_der = sensor_der.distance_cm()
+    distance_izq = sensor_izq.distance_cm()
+    if(distance_der < 5 or distance_izq < 5 ):
+        if (distance_der < 5):
+            led_der.on()
+
+        else:
+            led_der.off()
+
+        if (distance_izq < 5):
+            led_izq.on()
+
+        else:
+            led_izq.off()
+
     else:
-        led_verde.off()
-        led_rojo.on()
+        led_der.off()
+        led_izq.off()
     #print('Distance:', distance, 'cm')
     #sleep(1)
