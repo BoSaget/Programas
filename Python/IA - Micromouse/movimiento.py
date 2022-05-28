1c1968aa60acb5b29b388f3e7a58e5c47d2a4b0a
Leon <leontejeda@gmail.com>
2022-05-28 14:47:56 -0500

    Actualizacion 3 sensores y 2 motores

--- Python/IA - Micromouse/movimiento.py
+++ Python/IA - Micromouse/movimiento.py
@@ -10,9 +10,17 @@ led_cen = machine.Pin(21, machine.Pin.OUT)
 led_izq = machine.Pin(13, machine.Pin.OUT)
 
 # ESP32
+#Sensor Sonico
 sensor_der = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
 sensor_cen = HCSR04(trigger_pin=22, echo_pin=23, echo_timeout_us=10000)
 sensor_izq = HCSR04(trigger_pin=25, echo_pin=26, echo_timeout_us=10000)
+
+#Motores
+motor_der1 = machine.Pin(4, machine.Pin.OUT)
+motor_der2 = machine.Pin(2, machine.Pin.OUT)
+
+motor_izq1 = machine.Pin(14, machine.Pin.OUT)
+motor_izq2 = machine.Pin(12, machine.Pin.OUT)
 
 # ESP8266
 #sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
@@ -45,5 +53,9 @@ while True:
         led_der.off()
         led_cen.off()
         led_izq.off()
+
+        motor_der1.value(1)
+        motor_izq1.value(1)
+
     #print('Distance:', distance, 'cm')
     #sleep(1)
