ed8550e0e8335ef1346c86642e40131ceebd1b02
Leon <leontejeda@gmail.com>
2022-06-03 14:07:57 -0500

    Finalizado el perceptron Fase 1

--- Python/Perceptron_F1.py
+++ Python/Perceptron_F1.py
@@ -1,8 +1,7 @@
 import random
 
 def obtener_y(x0, x1, x2, pesos):
-    y = (float(x0)*float(pesos[0])) + (float(x1)*float(pesos[1])) + (float(x2)*float(pesos[2]))
-
+    y = (x0*pesos[0]) + (x1*pesos[1]) + (x2*pesos[2])
     #Funcion de activacion
     if (y >= 0):
         print("De esta iteracion se obtuvievo: " + str(y) + " por lo tanto se le asignara el valor de 1")
@@ -11,10 +10,11 @@ def obtener_y(x0, x1, x2, pesos):
         print("De esta iteracion se obtuvievo: " + str(y) + " por lo tanto se le asignara el valor de -1")
         return -1
 
-def reasignacion_pesos(bandera, x0, x1, x2, error, n, pesos):
-    pesos[0] = format(float(pesos[0]) + (float(x0[bandera])*error * n), '2f')
-    pesos[1] = format(float(pesos[1]) + (float(x1[bandera])*error * n), '2f')
-    pesos[2] = format(float(pesos[2]) + (float(x2[bandera])*error * n), '2f')
+def reasignacion_pesos(x0, x1, x2, error, n, pesos):
+    
+    pesos[0] = float(format(pesos[0] + (x0 * error * n), '.2f'))
+    pesos[1] = float(format(pesos[1] + (x1 * error * n), '.2f'))
+    pesos[2] = float(format(pesos[2] + (x2 * error * n), '.2f'))
 
     return pesos
     
@@ -33,7 +33,7 @@ n = 0.4
 
 #----------Definiendo DataSet----------
 x0 = [1, 1, 1, 1]
-x1 = [1, 1, -1 -1]
+x1 = [1, 1, -1, -1]
 x2 = [1, -1, 1, -1]
 y_des = [1, -1, -1, -1]
 
@@ -43,21 +43,19 @@ bandera = 0
 while bandera < 4:
 
     print("-----Iteracion numero: " + str(bandera + 1) + "-----")
-
-    y_obt = obtener_y(x0[bandera], x1[bandera], x2[bandera], w)
+    y_obt = obtener_y(float(x0[bandera]), float(x1[bandera]), float(x2[bandera]), w)
 
     error = int(y_des[bandera]) - y_obt
     
     if(error == 0):
         print("El error da un valor de: " + str(error) + " por lo tanto es valido")
+        bandera = bandera + 1
     else:
         print("El error da un valor de: " + str(error) + " por lo tanto no es valido, se recalcularan los pesos")
-        w = reasignacion_pesos(bandera, x0, x1, x2, error, n, w)
+        w = reasignacion_pesos(float(x0[bandera]), float(x1[bandera]), float(x2[bandera]), error, n, w)
         print("Los nuevos pesos seran: " + str(w))
-        bandera = -1
+        bandera = 0
 
-    enter = input()
-
-    bandera = bandera + 1
+    enter = input("\nPresione enter para continuar")
 
 
