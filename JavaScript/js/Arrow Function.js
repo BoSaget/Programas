65a12ae5515053fcf4a3dfddb7a39bc2f74dd1d8
leonSMN <leontejeda@gmail.com>
2022-07-19 12:16:06 -0500

    Curso JS

--- JavaScript/js/Arrow Function.js
+++ JavaScript/js/Arrow Function.js
@@ -1,6 +1,13 @@
 //Funciones - Arrow Function
 
-const sumar = numero1 => numero1 + 20
+const sumar = (numero1, numero2 = 1) => {
+    return numero1 + numero2
+}
 
 const resultado = sumar(30)
 console.log(resultado)
+
+const arrow = numero1 => numero1 + 2
+
+const resultado2 = arrow(20)
+console.log(resultado2)
