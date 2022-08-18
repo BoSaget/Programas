1b21bd94946c41a2c8b56b22d7dc1c189f3f4465
leonSMN <leontejeda@gmail.com>
2022-08-18 11:54:04 -0500

    Odometro Terminado

--- Proyectos/Odometro/Odometro.js
+++ Proyectos/Odometro/Odometro.js
@@ -1,3 +1,3 @@
 setTimeout(function(){
-    user.innerText = 1200//Endging ditit
+    user.innerText = 86//Endging ditit
 }, 500)//last argument is for time in milliseconds or seconds depending on the need
