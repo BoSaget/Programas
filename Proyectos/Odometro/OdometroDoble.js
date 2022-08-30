dd6fa27f0cf57aad4d2ea120a9d582a8a546ee6b
leonSMN <leontejeda@gmail.com>
2022-08-30 10:14:10 -0500

    Odometro con pausa y reinicio

---
+++ Proyectos/Odometro/OdometroDoble.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    user.innerText = 100//Endging ditit
+}, 500)//last argument is for time in milliseconds or seconds depending on the need
+
+setTimeout(3000)
+
+setTimeout(function(){
+    user.innerText = 850//Endging ditit
+}, 500)//last argument is for time in milliseconds or seconds depending on the need
