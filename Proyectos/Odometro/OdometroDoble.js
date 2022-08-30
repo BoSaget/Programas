e7527c66ccf5847013eb62580705bde08398b4c7
leonSMN <leontejeda@gmail.com>
2022-08-30 12:33:38 -0500

    Odometro Doble

--- Proyectos/Odometro/OdometroDoble.js
+++ Proyectos/Odometro/OdometroDoble.js
@@ -1,9 +1,10 @@
+console.log("Probando")
 setTimeout(function(){
     user.innerText = 100//Endging ditit
-}, 500)//last argument is for time in milliseconds or seconds depending on the need
+}, 1000)//last argument is for time in milliseconds or seconds depending on the need
 
-setTimeout(3000)
+setTimeout(console.log("Esperando..."),3000)
 
 setTimeout(function(){
     user.innerText = 850//Endging ditit
-}, 500)//last argument is for time in milliseconds or seconds depending on the need
+}, 1000)//last argument is for time in milliseconds or seconds depending on the need
