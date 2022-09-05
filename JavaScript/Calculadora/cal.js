4390e8555f8c398eaa19595a58b9ff7b1c72c64f
leonSMN <leontejeda@gmail.com>
2022-09-05 11:59:15 -0500

    Reordenamiento de Carpetas

---
+++ JavaScript/Calculadora/cal.js
@@ -0,0 +1,44 @@
+const botonNumero = document.getElementsByName("numero")
+//console.log(botonNumero)
+
+const botonOperador = document.getElementsByName("operador")
+//console.log(botonOperador)
+
+const botonClear = document.getElementsByName("clear")
+//console.log(botonClear)
+
+const botonResultado = document.getElementsByName("resultado")
+//console.log(botonResultado)
+
+let resultado = document.getElementById("resultado")
+
+botonNumero.forEach(function(boton)
+{
+    boton.addEventListener('click', function()
+    {
+        //agregarNumero(boton.innerText)
+        alert(boton.innerText)
+    })
+})
+
+/*
+botonOperador.forEach(function(boton)
+{
+    boton.addEventListener('click', function()
+    {
+        Operacion(boton.innerText)
+    })
+})
+
+botonClear.addEventListener("click", function()
+{
+    Borrar()
+    ActualizarDisplay()
+})
+
+botonResultado.addEventListener("click", function()
+{
+    Resultado()
+    ActualizarDisplay()
+})
+*/
