73f34a9cd51bd8b5a74d2beb16fd75ad80b38cdb
leonSMN <leontejeda@gmail.com>
2022-09-06 13:23:28 -0500

    CRUD CodeIgniter

--- Proyectos/CodeIgniter/app/Views/pages/pruebas.php
+++ Proyectos/CodeIgniter/app/Views/pages/pruebas.php
@@ -1,4 +1,29 @@
         <h1><del>Zona de pruebas</del></h1>
+        <h2>Probando Base de datos</h2>
+
+        <div>
+                <form action="./CRUD/insertar.php" method="get">
+                        <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Juan">
+                                <label for="nombre">Nombre</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Jimenez">
+                                <label for="apellido">Apellido</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                                <input type="number" class="form-control" id="celular" name="celular" placeholder="5544332211">
+                                <label for="celular">Numero</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                                <input type="number" class="form-control" id="boleta" name="boleta" placeholder="2020630XXX">
+                                <label for="boleta">Boleta</label>
+                        </div>
+                        <div class="col-12">
+                                <button class="btn btn-warning" type="submit">Submit form</button>
+                        </div>
+                </form>
+        </div>
         
         <div class="container">
                 <div class="row justify-content-center">
@@ -7,3 +32,5 @@
                         </div>
                 </div>
         </div>
+
+        
