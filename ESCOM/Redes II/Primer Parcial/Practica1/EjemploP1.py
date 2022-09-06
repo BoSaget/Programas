cb64f83fca5558641a5958e7aa85677edeb522d1
leonSMN <leontejeda@gmail.com>
2022-09-06 10:25:33 -0500

    Practica Redes 2 - Faltan Imagenes

--- ESCOM/Redes II/Primer Parcial/Practica1/EjemploP1.py
+++ ESCOM/Redes II/Primer Parcial/Practica1/EjemploP1.py
@@ -1,62 +1,85 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3.8
 #Autor: Ismael Lopez Larumbe
-#Sistemas Operativos
-import numpy as np
-import matplotlib.pyplot as plt
+#Redes 2
+import threading as th
 import skimage.io as io
 import os
 from os import getcwd
-from scandir import scandir
 from copy import deepcopy
 
-def separar(str_imagen):
+#En python no hay return convencional en los threads, se usan variables globales
+global canalesG
+canalesG=list()
+
+def filtroRG(str_imagen,indice):
 
     imagen = io.imread("origen/"+str_imagen)
     canal_rojo = deepcopy(imagen)
-    canal_verde = deepcopy(imagen)
+
+    canal_rojo[:,:,1]=0 #quita el verde
+    canal_rojo[:,:,2]=0 #quita el azul
+    #necesita numero entero, no acepta fracciones las coordenadas de las matrices
+    if imagen.shape[0]%2 == 0:
+        mitad=int(imagen.shape[0]/2)
+    else:
+        mitad=int((imagen.shape[0]+1)/2)
+
+    global canalesG #Accede a global de la mitad inferior
+    canalesG[indice][:mitad,:,0]=0 #quita el rojo
+    canalesG[indice][:mitad,:,2]=0 #quita el azul
+
+    nombre=str_imagen.split('.')[0] #obtiene nombre y quita la extension
+    io.imsave("resultados/"+nombre+"_rojo.png",canal_rojo)#Nombra y guarda la nueva imagen en la carpeta "resultados"
+
+def filtroBG(str_imagen,indice):
+
+    imagen = io.imread("origen/"+str_imagen)
     canal_azul = deepcopy(imagen)
 
-    canal_rojo[:,:,1]=0
-    canal_rojo[:,:,2]=0
+    canal_azul[:,:,0]=0 #quita el rojo
+    canal_azul[:,:,1]=0 #quita el verde
 
-    canal_verde[:,:,0]=0
-    canal_verde[:,:,2]=0
+    if imagen.shape[0]%2 == 0:
+        mitad=int(imagen.shape[0]/2)
+    else:
+        mitad=int((imagen.shape[0]-1)/2)
 
-    canal_azul[:,:,0]=0
-    canal_azul[:,:,1]=0
+    global canalesG#Accede a global de la mitad superior
+    canalesG[indice][mitad:,:,0]=0 #quita el rojo
+    canalesG[indice][mitad:,:,2]=0 #quita el azul
 
-    nombre=str_imagen.split('.')[0]
-    io.imsave("resultados/"+nombre+"_rojo.png",canal_rojo)
-    io.imsave("resultados/"+nombre+"_verde.png",canal_verde)
+    nombre=str_imagen.split('.')[0] #obtiene nombre y quita la extension
     io.imsave("resultados/"+nombre+"_azul.png",canal_azul)
 
+#obtiene los nombres de la carpeta "origen" como una lista
+def ls(ruta = getcwd()+"/origen"):
+    return [arch.name for arch in os.scandir(ruta) if arch.is_file()]
 
-def ls(ruta = getcwd()):
-    return [arch.name for arch in scandir(ruta) if arch.is_file()]
 
+nombres = ls()
+hilosR=list()
+hilosA=list()
 
-nombres = ls("/home/raderly/SO/imagenes_py/origen")
-print(nombres)
-print("Imagenes disponibles: "+str(len(nombres)))
+#prepara lista global,tendran acceso compartido
+for i in range(len(nombres)):
+    imagen = io.imread("origen/"+nombres[i])
+    canal_verde = deepcopy(imagen)
+    canalesG.append(canal_verde)
 
-while(True):
-    nproc=int(raw_input("Numero de procesos: "))
-    if(nproc > len(nombres)):
-        print("----Imagenes insuficientes----")
-    else:
-        break
+#Inicia los hilos y los mete a una lista
+for i in range(len(nombres)):
+    t1=th.Thread(name=(nombres[i].split('.')[0])+"_Rojo",target=filtroRG,args=(nombres[i],i, )) #Crea el hilo de filtro rojo y mitad verde
+    hilosR.append(t1)
+    t1.start()#Corre el hilo
+    t2=th.Thread(name=(nombres[i].split('.')[0])+"_Azul",target=filtroBG,args=(nombres[i],i, )) #crea el hilo de filtro azul y mitad verde
+    hilosA.append(t2)
+    t2.start()
 
-for i in range(nproc):
-    hijo=os.fork()
-    if hijo == 0:
-        separar(nombres[i]);
-        break
-    elif hijo > 0:
-        print("iteracion "+str(i))
-    else:
-        print("Error")
+#Junta los hilos y espera a que todos acaben
+for i in range(len(nombres)):
+    hilosA[i].join()
+    hilosR[i].join()
 
-if hijo>0:
-    for i in range(nproc):
-        print("Esperando")
-        os.wait()
+#Guarda como archivo los resultados de ambos hilos
+for i in range(len(nombres)):
+    io.imsave("resultados/"+nombres[i].split('.')[0]+"_verde.png",canalesG[i])
