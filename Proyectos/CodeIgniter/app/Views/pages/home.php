0a0ccc6413c861681178bcea7b55c4c338ed8a19
leonSMN <leontejeda@gmail.com>
2022-09-07 12:15:05 -0500

    Hey

--- Proyectos/CodeIgniter/app/Views/pages/home.php
+++ Proyectos/CodeIgniter/app/Views/pages/home.php
@@ -5,14 +5,14 @@
                         <div class="card text-bg-warning" style="max-width: 16rem;">
                                 <div class="card-header">HOLA!!</div>
                                 <div class="card-body text-bg-dark text-white">
-                                        <p class="card-text">Esta es una página en la que probaré varias cosas para poder hacer FullStack y ver como voy progresando</p>
+                                        <p class="card-text">Ja Que es esto!?</p>
                                 </div>
                         </div>
                 </div>
         
                 <div class="row justify-content-center">
                         <div class="col-auto">
-                                <a class="btn btn-outline-warning" href="pruebas">Pruebas</a>
+                                <a class="btn btn-outline-warning" href="insertar">CRUD</a>
                         </div>
                         <div class="col-auto">
                                 <a class="btn btn-outline-danger" href="about">About</a>
--- Proyectos/CodeIgniter/app/Views/pages/insertar.php
+++ Proyectos/CodeIgniter/app/Views/pages/insertar.php
@@ -2,28 +2,30 @@
         <h2>Probando Base de datos</h2>
 
         <div>
-                <form action="./CRUD/insertar.php" method="get">
+                <form action="<?=base_URL('insertar_controller/add');?>" method="get">
                         <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Juan">
+                                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre">
                                 <label for="nombre">Nombre</label>
                         </div>
                         <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Jimenez">
+                                <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido">
                                 <label for="apellido">Apellido</label>
                         </div>
                         <div class="form-floating mb-3">
-                                <input type="number" class="form-control" id="celular" name="celular" placeholder="5544332211">
+                                <input type="number" class="form-control" id="numero" name="numero" placeholder="Numero">
                                 <label for="celular">Numero</label>
                         </div>
                         <div class="form-floating mb-3">
-                                <input type="number" class="form-control" id="boleta" name="boleta" placeholder="2020630XXX">
+                                <input type="number" class="form-control" id="boleta" name="boleta" placeholder="Boleta">
                                 <label for="boleta">Boleta</label>
                         </div>
                         <div class="col-12">
-                                <button class="btn btn-warning" type="submit">Submit form</button>
+                                <button class="btn btn-warning" type="submit" name="submit" value="insertar">Enviar</button>
                         </div>
                 </form>
         </div>
+
+
         
         <div class="container">
                 <div class="row justify-content-center">
