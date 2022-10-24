0305dd901f72d4da0c8651ce48e2be99dd5d4cb3
Leon <leontejeda@gmail.com>
2022-10-23 21:14:03 -0500

    Formulario React

--- React/Citas Veterinario/src/components/Formulario.jsx
+++ React/Citas Veterinario/src/components/Formulario.jsx
@@ -10,27 +10,55 @@ function Formulario() {
           <span className="text-blue-600">
             Administrar
           </span>
-
-          <form className="text-xl bg-gray-300 shadow-lg rounded-md mt-3 pt-2 pb-4">
-            <div className="text-lg-center text-blue-400">
+        </p>
+        <div className="font-bold text-xl text-center mt-3 pt-2 pb-4">
+          <form className=" bg-gray-300 shadow-lg rounded-md pt-1">
+            <div className="text-lg text-blue-400">
               <label class="block mt-1"htmlFor="mascota">Nombre Mascota</label>
               <input 
                 id="mascota"
                 type="text"
                 placeholder="Nombre de la mascota" 
-                className=" bg-gray-300 rounded-lg border-2 border-black outline-green-500 placeholder-blue-400 mt-2 p-2"
+                className=" bg-gray-300 rounded-lg border-2 border-black outline-blue-400 placeholder-blue-400 my-2 p-2 w-80"
               />
 
-              <label class="block mt-1"htmlFor="2">Buebos</label>
+              <label class="block mt-1"htmlFor="propietario">Propietario</label>
               <input 
-                id="2"
+                id="propietario"
                 type="text"
-                placeholder="Be" 
-                className=" bg-gray-300 rounded-lg border-2 border-black outline-green-500 placeholder-blue-400 mt-2 p-2"
+                placeholder="Propietario" 
+                className=" bg-gray-300 rounded-lg border-2 border-black outline-blue-400 placeholder-blue-400 my-2 p-2 w-80"
+              />
+
+              <label class="block mt-1"htmlFor="email">Email</label>
+              <input 
+                id="email"
+                type="email"
+                placeholder="Email de contacto" 
+                className=" bg-gray-300 rounded-lg border-2 border-black outline-blue-400 placeholder-blue-400 my-2 p-2 w-80"
+              />
+
+              <label class="block mt-1"htmlFor="alta">Fecha de alta</label>
+              <input 
+                id="alta"
+                type="date"
+                className=" bg-gray-300 rounded-lg border-2 border-black outline-blue-400 placeholder-blue-400 my-2 p-2 w-80"
+              />
+
+              <label class="block mt-1"htmlFor="alta">Fecha de alta</label>
+              <textarea
+                id="sintomas"
+                className=" bg-gray-300 rounded-lg border-2 border-black outline-blue-400 placeholder-blue-400 my-2 p-2 w-80"
+                placeholder="Describe los sintomas"
               />
             </div>
+            <input
+              type="submit"
+              className="font-bold uppercase  bg-gray-500 rounded-lg  w-80 p-3 mb-2 hover:bg-green-600"
+              value="Agregar Paciente"
+            />
           </form>
-        </p>
+        </div>
     </div>
   )
 }
