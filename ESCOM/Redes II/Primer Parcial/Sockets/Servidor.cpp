ae0e554945dac4b44344e037e88c9b25e5bb8742
Leon <leontejeda@gmail.com>
2022-10-23 21:19:57 -0500

    Sockets redes

--- ESCOM/Redes II/Primer Parcial/Sockets/Servidor.cpp
+++ ESCOM/Redes II/Primer Parcial/Sockets/Servidor.cpp
@@ -1,65 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
 
-int maint(int argc, char ** argv)
-{
-    //Index
+int main(int argc, char *argv[]) {
+    // Index
     char buffer[100];
-    
-    if(argc < 3)
-    {
-        printf("Falto indtroducir dirección ip y puerto \n");
+    if(arcg < 3){
+        printf("Falto introducir direccion ip y puerto\n");
         exit(-1);
     }
 
     int puerto = atoi(argv[2]);
 
-    //Paso 1 Crecion del socket
-    int mysocket = socket(AF_INET, SOCK_STREAM, 0);
+    // Paso 1. Creacion del socket
+    int mySocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(mysocket < 0)
-    {
-        printf("Error al abrir socket \n");
+    if(mySocket < 0) {
+        printf("Error al abrir socket\n");
         exit(-1);
-    } 
+    }
 
-    //Paso 2 Establecer la conexión hacia el servidor
-    struct sockaddr_in servidor;
-
-    //AFINET familia de prococolos
+    // Paso 2. Hacer publico el servicio
+    struct sockaddr_in servidor, cliente;
     servidor.sin_family = AF_INET;
     servidor.sin_port = htons(puerto);
-
-    //Pasa la cadena de formato char a formato de red
     servidor.sin_addr.s_addr = inet_pton(AF_INET, argv[1], &(servidor.sin_addr));
+    //char str[INET_ADDRSTRLEN];
+    //servidor.sin_addr = inet_ntop(AF_INET, &(servidor.sin_addr), str, INET_ADDRSTRLEN);
+    int isBind = bind(mySocket, struct sockaddr*(&servidor), sizeof(servidor));
 
-    /*Para pasar la cadnea de formato de red a formato de char
-    char str(INET_ADDRSSTRLEN);
-    servidor.sin_addr = inet_ntop(AF_INET, &(servidor.sin_addr), str INET_ADDRSSTRLEN);
-    */
+    if (isBind == -1) {
+        printf("No se establecio la conexion con el servidor\n");
+        exit(-1);
+    }
 
-   int isConnect = bind(mysocket, struct sockaddr *(&servidor), sizeof(servidor));
+    // Paso 3. Encolar las peticiones
+    listen(mySocket, 4);
+    while(true) {
 
-   if(isConnect == -1)
-   {
-    printf("No se ha establecido la conexión con el servidor \n");
-    exit(-1);
-   }
+        // Paso 5. Canal de conexion
+        int tamCliente = sizeof(cliente);
+        int canal = accept(mySocket, struct sockaddr*(&cliente), &tamCliente); //ID del canal
 
-   //Paso 3 Fase de comunicación
-   strcpy(buffer, "Hola desde cliente");
-   send(mysocket, buffer, strlen(buffer), 0);
+        if(canal == -1) {
+            printf("No se establecio el canal");
+            exit(-1);
+        }
 
-   //Paso 4 Recibir el mensaje
-   int mensaje = recv(mysocket, buffer, sizeof(buffer), 0);
-    buffer[mensaje] = '\0';
+        //Paso 6. Recibir
+        int tam = recv(canal, buffer, sizeof(buffer), 0);
+        buffer[tam] = '\0';
+        printf("Mensaje del cliente: %s\n", buffer);
 
-    printf("Mensaje recibido por el servidor");
-    close(mysocket);
+        // Paso 7. Enviar la respuesta del servidor
+        strcpy(buffer, "Soy servidor");
+        int tam = send(canal, buffer, strlen(buffer), 0);
+        close(canal);
+    }
 
-return 0;
+    return 0;
 }
