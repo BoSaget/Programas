6a4c6fa879be79ff1b974327c6f3a7156387e866
Leon <leontejeda@gmail.com>
2022-11-12 18:11:21 -0600

    Puntos en el mapa

---
+++ Proyectos/Punteros en Mapa/index.js
@@ -0,0 +1,21 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const marcador = { lat: 21.03832, lng:  -101.58679};
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: marcador,
+    });
+
+    const verde = "./Puntos/verde.png"
+
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: marcador,
+      map: map,
+      icon: verde
+    });
+  }
+  
+  window.initMap = initMap;
