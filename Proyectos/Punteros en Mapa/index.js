c7cffd48d573f7c1e605ab1059bc5006eb0fa53f
Leon <leontejeda@gmail.com>
2022-11-13 22:11:34 -0600

    Arreglo de iteradores

--- Proyectos/Punteros en Mapa/index.js
+++ Proyectos/Punteros en Mapa/index.js
@@ -1,21 +1,30 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const marcador = { lat: 21.03832, lng:  -101.58679};
+    const marcadores = [
+      [21.03832, -101.58679, "./Puntos/rojo.png"],
+      [21.02754, -101.56811, "./Puntos/rojo.png"],
+      [21.065568, -101.634039, "./Puntos/verde.png"]
+    ]
+
+    const centro = {lat: 23.00000, lng: -102.00000}
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: marcador,
+      zoom: 6,
+      center: centro
     });
 
-    const verde = "./Puntos/rojo.png"
-
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: marcador,
-      map: map,
-      icon: verde     
+    // The marker, positioned at Uluru  
+    marcadores.forEach(marcador => {
+      console.log(marcador)
+      const coord = {lat: marcador[0], lng:marcador[1]}
+      const marker = new google.maps.Marker({
+        position: coord,
+        map: map,
+        icon: marcador[2]
+      });
     });
+
   }
   
   window.initMap = initMap;
