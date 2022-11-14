b521f548089c7ca2c5fb6405857361e86a3e2b9c
Leon <leontejeda@gmail.com>
2022-11-13 23:21:30 -0600

    Mapa ejecutandose cada 2 segundos

--- Proyectos/Punteros en Mapa/index.js
+++ Proyectos/Punteros en Mapa/index.js
@@ -1,30 +1,32 @@
-// Initialize and add the map
+// Inicia y presenta el mapa
 function initMap() {
-    // The location of Uluru
-    const marcadores = [
-      [21.03832, -101.58679, "./Puntos/rojo.png"],
-      [21.02754, -101.56811, "./Puntos/rojo.png"],
-      [21.065568, -101.634039, "./Puntos/verde.png"]
-    ]
+  //marcadores es una matriz de 2 dimensiones en donde se guardan las coordenadas y el estatus
+  const marcadores = [
+    [21.03832, -101.58679, "./Puntos/rojo.png"],
+    [21.02754, -101.56811, "./Puntos/rojo.png"],
+    [21.065568, -101.634039, "./Puntos/verde.png"]
+  ]
 
-    const centro = {lat: 23.00000, lng: -102.00000}
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 6,
-      center: centro
+  const centro = {lat: 23.00000, lng: -102.00000}
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: centro
+  });
+
+  // la constante marker se encarga de poner el marcador
+  marcadores.forEach(marcador => {
+    console.log(marcador)
+    const coord = {lat: marcador[0], lng:marcador[1]}
+    const marker = new google.maps.Marker({
+      position: coord,
+      map: map,
+      icon: marcador[2]
     });
+  });
 
-    // The marker, positioned at Uluru  
-    marcadores.forEach(marcador => {
-      console.log(marcador)
-      const coord = {lat: marcador[0], lng:marcador[1]}
-      const marker = new google.maps.Marker({
-        position: coord,
-        map: map,
-        icon: marcador[2]
-      });
-    });
-
-  }
+}
+window.initMap = initMap
+//setInterval ejecuta el comando cada  espera 5 segundos
+const intervalo = setInterval(initMap, 4000, window.initMap)
   
-  window.initMap = initMap;
