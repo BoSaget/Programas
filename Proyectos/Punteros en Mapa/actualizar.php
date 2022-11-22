08864cc8f0024c0a901b9bf41a7dc2f5cbe3ba5e
Leon <leontejeda@gmail.com>
2022-11-21 23:57:37 -0600

    Grafica puntos, solo falta actualizar

--- Proyectos/Punteros en Mapa/actualizar.php
+++ Proyectos/Punteros en Mapa/actualizar.php
@@ -2,7 +2,7 @@
     $conexion = mysqli_connect("localhost","root","","pruebas");
     mysqli_query($conexion, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 
-    $sql = "SELECT * FROM puntos";
+    $sql = "SELECT * FROM puntos WHERE estatusAnterior <> estatusActual";
     $resultado = mysqli_query($conexion,$sql);
     $info = mysqli_num_rows($resultado);
     $columna = mysqli_fetch_row($resultado);
@@ -10,16 +10,19 @@
     $data = array();
     $puntero = array();
 
+    //Se inicializa el valor de i para para acomodar el areglo
     $i=0;
     while ($datos = mysqli_fetch_array($resultado))
     {
+        $sqlUpdate = "UPDATE puntos SET estatusAnterior = ". $datos[3] . "  WHERE id = " . $datos[0];
+        $update = mysqli_query($conexion,$sqlUpdate);
+
         //Se acumulan las coordenadas en una sola cadena para despues separarlas
-        $puntero[$i] = $datos[1] . '_' . $datos[2] . '_' . $datos[3]; 
+        $puntero[$i] = $datos[1] . '_' . $datos[2] . '_' . $datos[3] . $datos[4];
         $i++;
         
     }
 
-    
     $data["punto"] = $puntero;
     $data["codigo"] = 0;
     if($info > 1)
--- Proyectos/Punteros en Mapa/index.js
+++ Proyectos/Punteros en Mapa/index.js
@@ -1,39 +1,99 @@
+let marcadores
+let mapa = 0
+let cambioDeEstatus = false
+
 // Inicia y presenta el mapa
-function initMap() {
-  //marcadores es una matriz de 2 dimensiones en donde se guardan las coordenadas y el estatus
-  const marcadores = [
-    [21.03832, -101.58679, "./Puntos/rojo.png"],
-    [21.02754, -101.56811, "./Puntos/rojo.png"],
-    [21.065568, -101.634039, "./Puntos/verde.png"]
-  ]
+function initMap() 
+{  
+  if(cambioDeEstatus == false)
+  {
+    //Se define el punto desde el que se apreciará el mapa por default y la coordenada para centrar
+    const centro = {lat: 23.00000, lng: -102.00000}
+    mapa = new google.maps.Map(document.getElementById("mapa"), {
+      zoom: 6,
+      center: centro
+    });
 
-  const centro = {lat: 23.00000, lng: -102.00000}
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 6,
-    center: centro
-  });
+    //Cambia el estatus, para que no se actualiza todo el mapa
+    cambioDeEstatus = true
+    console.log("entró if " + cambioDeEstatus)
+  }
 
-  // la constante marker se encarga de poner el marcador
+  else
+  {
+    console.log("entró else")
+    mapa.setZoom(mapa.getZoom());
+  }
+
+  console.log(marcadores)
+  //La variable global marcadores entra en un ciclo para extrar cada elemento del arrelgo
   marcadores.forEach(marcador => {
+    
+    //Se separa la cadena de valores   
+    marcador= marcador.split('_')
     console.log(marcador)
-    const coord = {lat: marcador[0], lng:marcador[1]}
+
+    //Guarda el valor de la coordenada en una constante
+    const coord = {lat: parseFloat(marcador[0]), lng: parseFloat(marcador[1])}
+
+    let color = "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+
+    //Checa el estatus para definir el color del puntero
+    if(marcador[2] == '1')
+    {
+      color = "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+    }
+
+    //Crea el mapa con el color del estatus y la coordenada pasadas
     const marker = new google.maps.Marker({
       position: coord,
-      map: map,
-      icon: marcador[2]
+      map: mapa,
+      icon: color
     });
+
   });
-
 }
-window.initMap = initMap
 
-$.ajax({
-  url: 'index.php',
-  success: function(data) {
-    $('.result').html(data);
-  }
-});
-//setInterval ejecuta el comando cada  espera 5 segundos
-const intervalo = setInterval(initMap, 4000, window.initMap)
+function actualizarEstatus()
+{
+  $.ajax({
+    url: 'actualizar.php',
+    success: function(data) {
   
+      //Respuesta extrae el valor de la base de datos y lo pasa a la variable global marcadores como arrelgo
+      const respuesta = JSON.parse(data)
+      marcadores = respuesta.punto
+
+      if(marcadores.length != 0)
+      {
+        console.log(cambioDeEstatus)
+        
+        window.initMap = initMap
+        window.location.replace(window.location) 
+      }
+    }
+  })
+}
+
+if (mapa == 0)
+{
+  $.ajax({
+    url: 'index.php',
+    success: function(data) {
+  
+      //Respuesta extrae el valor de la base de datos y lo pasa a la variable global marcadores como arrelgo
+      const respuesta = JSON.parse(data)
+      marcadores = respuesta.punto
+  
+      //Manda a llamar a la funcion para mostrar el mapa
+      window.initMap = initMap
+      
+    }
+  })
+}
+
+//Funcion para actualizar los punteros
+setInterval(
+  actualizarEstatus,
+  3000
+)
