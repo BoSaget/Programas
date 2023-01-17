6ae2fa2b85606103256394239f9310ece380c88d
LeonLinux <leontejeda@gmail.com>
2023-01-16 19:20:45 -0600

    Arreglo de pines de ESP

--- ESCOM/Instrumentacion/Proyecto.py
+++ ESCOM/Instrumentacion/Proyecto.py
@@ -1,31 +1,24 @@
-from machine import Pin
+import machine
 import time
 
 #Las GPIO son del 36 al 26
 
 while True:
     ## 1110 0001 Temperatura ambiente
-    uno = Pin(36, machine.Pin.IN)
-    dos = Pin(39, machine.Pin.IN)
-    cuatro = Pin(34, machine.Pin.IN)
-    ocho = Pin(35, machine.Pin.IN)
+    uno = machine.Pin(34, machine.Pin.IN)
+    dos = machine.Pin(35, machine.Pin.IN)
+    cuatro = machine.Pin(32, machine.Pin.IN)
+    ocho = machine.Pin(33, machine.Pin.IN)
     
-    diezSeis = Pin(32, machine.Pin.IN)
-    treintaDos = Pin(23, machine.Pin.IN)
-    sesentaCuatro = Pin(25, machine.Pin.IN)
-    cientoVeinteOcho = Pin(26, machine.Pin.IN)
+    diezSeis = machine.Pin(25, machine.Pin.IN)
+    treintaDos = machine.Pin(26, machine.Pin.IN)
+    sesentaCuatro = machine.Pin(27, machine.Pin.IN)
+    cientoVeinteOcho = machine.Pin(14, machine.Pin.IN)
     
-    if (uno.value() == 1 and dos.value() == 0 and cuatro.value() == 0 and ocho.value() == 0 and diezSeis.value() == 0 and treintaDos.value() == 1 and cientoVeinteOcho.value() == 1):
+    if (uno.value() == 1 and dos.value() == 0 and cuatro.value() == 1 and ocho.value() == 0 and diezSeis.value() == 1 and treintaDos.value() == 0):
         print("No hay humedad, humedad ambiente")
+        machine.Pin(2, machine.Pin.OUT).value(0)
         
-    elif (uno.value() == 1 and ocho.value() == 1):
+    elif (uno.value() == 1 and dos.value() == 1 and cuatro.value() == 1 and ocho .value() == 1 and diezSeis.value() == 1):
         print("Humedo")
-        
-    time.sleep(2)
-    
-    
-    
-
-
-
-
+        machine.Pin(2, machine.Pin.OUT).value(1)
