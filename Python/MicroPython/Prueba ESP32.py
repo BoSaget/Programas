8c5dc3a15175bf70c6bd01127cecaec4193c944d
Leon <leontejeda@gmail.com>
2023-10-20 00:38:33 -0600

    Modificaicón Led parpadeante ESP 32 Micropython

--- Python/MicroPython/Prueba ESP32.py
+++ Python/MicroPython/Prueba ESP32.py
@@ -1,2 +1,11 @@
 from machine import Pin
-Pin(2, Pin.OUT).value(1)
+import time
+
+while True:
+    #Prende el led azul de la ESP
+    Pin(2, Pin.OUT).value(1)
+    time.sleep(1)
+    
+    #Apaga el led azul de la ESP
+    Pin(2, Pin.OUT).value(0)
+    time.sleep(1)
