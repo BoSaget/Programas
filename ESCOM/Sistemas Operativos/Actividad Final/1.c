6422d060b5f25db2ddfa436bd6a5a76d8eb93eea
Leon <leontejeda@gmail.com>
2024-01-01 20:19:23 -0600

    Codigo 1 SO

--- ESCOM/Sistemas Operativos/Actividad Final/1.c
+++ ESCOM/Sistemas Operativos/Actividad Final/1.c
@@ -1,33 +1,39 @@
-#include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //Define las caracteristicas de implementación de POSIX
+#include <sys/types.h> //Contiene los tipos de variables para ocupar
+#include <sys/ipc.h> //Contiene las estructuras para las subrutinas
+#include <sys/shm.h> //Define estructuras que se utilizan para la memoria compartida
+#include <sys/wait.h> // Para poder utilizar l afunción wait
 
-int main (int argc, char *argv[])
-{
-    pid_t pid;
-    int status;
+int main() {
+  int pid1, pid2;
 
-    pid = fork ();
-    if( pid != 0)
+  pid1 = fork();
+  if (pid1 == 0)
+  {
+    for(int i = 0; i <= 9; i++)
     {
-        while(pid != wait(&status));
+        printf("Proceso hijo 1: %d\n", i+1);
     }
+    return 0;
+  }
 
-    else
+  pid2 = fork();
+  if (pid2 == 0) 
+  {
+    for(int j = 0; j <= 9; j++)
     {
-        sleep(5);
-        exit(5);
+        printf("Proceso hijo 2: %d\n", j+1);
     }
+    return 0;
+  }
 
-    pid = fork();
-    if(pid != 0)
-    {
-        while(pid != wait(&status));
-    }
+  // Espera a que los procesos hijos terminen
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
 
-    else
-    {
-        sleep(1);
-        exit(1);
-    }
+  printf("Proceso padre: Terminado\n");
 
+  return 0;
 }
